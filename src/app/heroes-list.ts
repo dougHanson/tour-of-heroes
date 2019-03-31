8cc94c4f3fcf46b53d32c1dfc4f7f742da7f25ac
Doug Hanson <doug83@iinet.net.au>
2019-03-31 19:00:26 +0800

    tour-of-heroes

---
+++ src/app/heroes-list.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Doug' },
+  { id: 2, name: 'Andrea' },
+  { id: 33, name: 'Shitface' },
+  { id: 14, name: 'Rasputen' },
+  { id: 250, name: 'Esmerelda' }
+];
